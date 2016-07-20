4e66e6f97f03d54419aa7cf3636f75e420fe6abf
Jay Wang <jaywang@codeaurora.org>
2016-07-19 18:11:05 -0700

    SnapdragonCamera: Remove custom location permission request window
    When user enables the GPS locaion setting, use Android's run-time
    permission request interface to get the permission.
    
    Change-Id: Ib17171aeff5ccf20e2b00da08243dee1a3383611
    CRs-Fixed: 1038039

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -85,6 +85,7 @@ public class VideoModule implements CameraModule,
     FocusOverlayManager.Listener,
     CameraPreference.OnPreferenceChangedListener,
     ShutterButton.OnShutterButtonListener,
+    LocationManager.Listener,
     MediaRecorder.OnErrorListener,
     MediaRecorder.OnInfoListener {
 
@@ -491,7 +492,7 @@ public class VideoModule implements CameraModule,
         mUI.setPrefChangedListener(this);
 
         mQuickCapture = mActivity.getIntent().getBooleanExtra(EXTRA_QUICK_CAPTURE, false);
-        mLocationManager = new LocationManager(mActivity, null);
+        mLocationManager = new LocationManager(mActivity, this);
 
         mUI.setOrientationIndicator(0, false);
         setDisplayOrientation();
@@ -557,6 +558,23 @@ public class VideoModule implements CameraModule,
             mCameraDevice.setParameters(mParameters);
         }
     }
+
+    @Override
+    public void waitingLocationPermissionResult(boolean result) {
+        mLocationManager.waitingLocationPermissionResult(result);
+    }
+
+    @Override
+    public void enableRecordingLocation(boolean enable) {
+        String value = (enable ? RecordLocationPreference.VALUE_ON
+                        : RecordLocationPreference.VALUE_OFF);
+        if (mPreferences != null) {
+            mPreferences.edit()
+                .putString(CameraSettings.KEY_RECORD_LOCATION, value)
+                .apply();
+        }
+        mLocationManager.recordLocation(enable);
+     }
 
     // SingleTapListener
     // Preview area is touched. Take a picture.
@@ -2996,4 +3014,10 @@ public class VideoModule implements CameraModule,
         resumeVideoRecording();
     }
 
+    @Override
+    public void onErrorListener(int error) {
+        enableRecordingLocation(false);
+    }
+
 }
+
